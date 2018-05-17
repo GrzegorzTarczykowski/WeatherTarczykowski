0ca94c6bd8ab676cce561f602842451fc439552a
GrzegorzTarczykowski <grzegorz.tarczykowski@gmail.com>
2018-05-18 01:16:41 +0200

    Next Commit

---
+++ WeatherTarczykowski/WeatherTarczykowski/CurrentWeather.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherTarczykowski
+{
+    public class CurrentWeather
+    {
+        public string CityName { get; set; }
+        public string Temperature { get; set; }
+        public string Sunrise { get; set; }
+        public string Sunset { get; set; }
+        public string Humidity { get; set; }
+        public string Pressure { get; set; }
+        public string WindSpeedValue { get; set; }
+        public string WindSpeedName { get; set; }
+        public string WindDirectionCode { get; set; }
+        public string WindDirectionName { get; set; }
+        public string CloudsName { get; set; }
+    }
+}
--- WeatherTarczykowski/WeatherTarczykowski/MainPage.xaml.cs
+++ WeatherTarczykowski/WeatherTarczykowski/MainPage.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Xml.Linq;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +24,44 @@ namespace WeatherTarczykowski
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        const string dateOpenweathermap = "http://api.openweathermap.org/data/2.5/weather?q=Bydgoszcz&lang=pl&units=metric&type=like&mode=xml&appid=5e79a54fbe78a6ba372226ca3e136581";
+        List<CurrentWeather> listOfCurrentWeather = new List<CurrentWeather>();
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            var serverOpenweathermap = new HttpClient();
+            string date = "";
+            try
+            {
+                date = await serverOpenweathermap.GetStringAsync(new Uri(dateOpenweathermap));
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            if (date != "")
+            {
+                XDocument dateOfWeather = XDocument.Parse(date);
+                listOfCurrentWeather = (from item in dateOfWeather.Descendants("current")
+                         select new CurrentWeather()
+                         {
+                             CityName = (item.Element("city").Attribute("name").Value),
+                             Temperature = (item.Element("temperature").Attribute("value").Value),
+                             Sunrise = (item.Element("city").Element("sun").Attribute("rise").Value),
+                             Sunset = (item. Element("city").Element("sun").Attribute("set").Value),
+                             Humidity = (item.Element("humidity").Attribute("value").Value),
+                             Pressure = (item.Element("pressure").Attribute("value").Value),
+                             WindSpeedValue = (item.Element("wind").Element("speed").Attribute("value").Value),
+                             WindSpeedName = (item.Element("wind").Element("speed").Attribute("name").Value),
+                             WindDirectionCode = (item.Element("wind").Element("direction").Attribute("code").Value),
+                             WindDirectionName = (item.Element("wind").Element("direction").Attribute("name").Value),
+                             CloudsName = (item.Element("clouds").Attribute("name").Value)
+                         }).ToList();
+            }
         }
     }
 }

7a5df8d7da20885d7592239b15e84656dab55d47
GrzegorzTarczykowski <grzegorz.tarczykowski@gmail.com>
2018-06-05 02:13:37 +0200

    music, storage, image, aboutApplication

---
+++ WeatherTarczykowski/WeatherTarczykowski/AboutApplication.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherTarczykowski
+{
+    class AboutApplication
+    {
+        public static string aboutApplication = "Autor: Grzegorz Tarczykowski \n" +
+                                                "Aplikacja ta służy do pobierania i wyświetlania \n" +
+                                                "aktualnej prognozy pogody dla wskazanego miasta. \n" +
+                                                "Dane o pogodzie pobierane są ze strony \n" +
+                                                "openweathermap.org. Aplikacja wykorzystuje w tym \n" +
+                                                "celu udostępniane na wyżej wymienionej stronie API \n" +
+                                                "w formacie XML. Aplikacja przechowuje w pamięci \n" +
+                                                "najczęściej wyszukiwane ostatnie miasta. \n"+
+                                                "Aplikacja wyświetla wschód i zachód słońca \n"+
+                                                "Aplikacja odtwarza w tle przyjemną muzyke.";
+    }
+}
--- WeatherTarczykowski/WeatherTarczykowski/MainPage.xaml.cs
+++ WeatherTarczykowski/WeatherTarczykowski/MainPage.xaml.cs
@@ -18,6 +18,7 @@ using WeatherTarczykowski;
 using Windows.UI.Popups;
 using Windows.Storage;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 //Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x415
 
@@ -26,21 +27,58 @@ namespace WeatherTarczykowski
     /// <summary>
     /// Pusta strona, która może być używana samodzielnie lub do której można nawigować wewnątrz ramki.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
     {
         const string firstPartDateOpenweathermap = "http://api.openweathermap.org/data/2.5/weather?q=";
         const string secoundPartDateOpenweathermap = "&lang=pl&units=metric&type=like&mode=xml&appid=5e79a54fbe78a6ba372226ca3e136581";
-        List<CurrentWeather> listOfCurrentWeather = new List<CurrentWeather>();
+        CurrentWeather currentWeather = new CurrentWeather();
         ObservableCollection<string> listOfMostSearchedCity = new ObservableCollection<string>();
         Dictionary<string, int> dictionaryOfMostSearched = new Dictionary<string, int>();
+        string rootPath = Windows.ApplicationModel.Package.Current.InstalledLocation.Path;
+        MediaElement mediaHandler = new MediaElement();
         public MainPage()
         {
             this.InitializeComponent();
+            DataContext = this;
+            Application.Current.Suspending += Current_Suspending;
+            textBlockInformationAboutApplication.Text = AboutApplication.aboutApplication;
         }
 
-        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        private void Current_Suspending(object sender, Windows.ApplicationModel.SuspendingEventArgs e)
         {
-            dictionaryOfMostSearched.Add("Bydgoszcz", 1);
+            var top3tmp = listOfMostSearchedCity.Take(3);
+            ApplicationData.Current.LocalSettings.Values.Clear();
+            int i = 3;
+            foreach (string item in top3tmp)
+            {
+                ApplicationData.Current.LocalSettings.Values.Add(item, i--);
+            }
+        }
+
+        private async void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            ((PivotItem)pivot.Items[1]).IsEnabled = false;
+            var folder = await StorageFolder.GetFolderFromPathAsync(rootPath + @"\Assets\Music");
+            if(folder != null)
+            {
+                var file = await folder.GetFileAsync("Shakin Stevens - Snow is falling.mp3");
+                if(file != null)
+                {
+                    var stream = await file.OpenReadAsync();
+                    mediaHandler.SetSource(stream, file.ContentType);
+                    mediaHandler.Volume = 100;
+                    mediaHandler.Play();
+                }
+            }
+            if(ApplicationData.Current.LocalSettings.Values.Count != 0)
+            {
+                dictionaryOfMostSearched.Clear();
+                foreach (var item in ApplicationData.Current.LocalSettings.Values)
+                {
+                    dictionaryOfMostSearched.Add(item.Key, (int)item.Value);
+                }
+            }
+            //dictionaryOfMostSearched.Add("Bydgoszcz", 1);
             var top3 = dictionaryOfMostSearched.OrderByDescending(pair => pair.Value).Take(3);
             listOfMostSearchedCity.Clear();
             foreach (var item in top3)
@@ -48,6 +86,57 @@ namespace WeatherTarczykowski
                 listOfMostSearchedCity.Add(item.Key);
             }
             listBoxMostSearched.ItemsSource = listOfMostSearchedCity;
+        }
+
+        private string ReturnWindDirectionName (string windDirectionCode)
+        {
+            string windDirectionName = "";
+            switch(windDirectionCode)
+            {
+                case "N":
+                    windDirectionName = "Połnocny";
+                    break;
+                case "E":
+                    windDirectionName = "Wschodni";
+                    break;
+                case "S":
+                    windDirectionName = "Połódniowy";
+                    break;
+                case "W":
+                    windDirectionName = "Zachodni";
+                    break;
+                case "NE":
+                    windDirectionName = "Połnocno-Wschodni";
+                    break;
+                case "NW":
+                    windDirectionName = "Połnocno-Zachodni";
+                    break;
+                case "SE":
+                    windDirectionName = "Połódniowo-Wschodni";
+                    break;
+                case "SW":
+                    windDirectionName = "Połódniowo-Zachodni";
+                    break;
+                case "NNE":
+                    windDirectionName = "Północno-Północno-Wschodni";
+                    break;
+                case "NNW":
+                    windDirectionName = "Północno-Północno-Zachodni";
+                    break;
+                case "SSE":
+                    windDirectionName = "Połódniowo-Połódniowo-Wschodni";
+                    break;
+                case "SSW":
+                    windDirectionName = "Połódniowo-Połódniowo-Zachodni";
+                    break;
+            }
+            return windDirectionName;
+        }
+
+        private string ReturnSunriseOrSunset(string x)
+        {
+            string sunriseorsunset = string.Format("{0}{1}.{2}{3}.{4}{5}{6}{7} {8}{9}:{10}{11}:{12}{13}", x[8], x[9], x[5], x[6], x[0], x[1], x[2], x[3], x[11], x[12], x[14], x[15], x[17], x[18]);
+            return sunriseorsunset;
         }
 
         private async void buttonSearchWeather_Click(object sender, RoutedEventArgs e)
@@ -90,21 +179,21 @@ namespace WeatherTarczykowski
                         }
                     }
                     XDocument dateOfWeather = XDocument.Parse(date);
-                    listOfCurrentWeather = (from item in dateOfWeather.Descendants("current")
-                                            select new CurrentWeather()
-                                            {
-                                                CityName = (item.Element("city").Attribute("name").Value),
-                                                Temperature = (item.Element("temperature").Attribute("value").Value),
-                                                Sunrise = (item.Element("city").Element("sun").Attribute("rise").Value),
-                                                Sunset = (item.Element("city").Element("sun").Attribute("set").Value),
-                                                Humidity = (item.Element("humidity").Attribute("value").Value),
-                                                Pressure = (item.Element("pressure").Attribute("value").Value),
-                                                WindSpeedValue = (item.Element("wind").Element("speed").Attribute("value").Value),
-                                                WindSpeedName = (item.Element("wind").Element("speed").Attribute("name").Value),
-                                                WindDirectionCode = (item.Element("wind").Element("direction").Attribute("code").Value),
-                                                WindDirectionName = (item.Element("wind").Element("direction").Attribute("name").Value),
-                                                CloudsName = (item.Element("clouds").Attribute("name").Value)
-                                            }).ToList();
+                    var tmpList = (from item in dateOfWeather.Descendants("current")
+                                   select new CurrentWeather()
+                                   {
+                                       CityName = (item.Element("city").Attribute("name").Value),
+                                       Temperature = "Temperatura: " + (item.Element("temperature").Attribute("value").Value) + "°C",
+                                       Sunrise = "Wschód słońca: \n" + ReturnSunriseOrSunset((item.Element("city").Element("sun").Attribute("rise").Value)),
+                                       Sunset = "Zachód słońca: \n" + ReturnSunriseOrSunset((item.Element("city").Element("sun").Attribute("set").Value)),
+                                       Humidity = "Wilgotność: " + (item.Element("humidity").Attribute("value").Value) + "%",
+                                       Pressure = (item.Element("pressure").Attribute("value").Value),
+                                       WindSpeedValue = "Prędkość wiatru: " + (item.Element("wind").Element("speed").Attribute("value").Value) + " m/s",
+                                       WindDirectionCode = "(" + (item.Element("wind").Element("direction").Attribute("code").Value) + ")",
+                                       WindDirectionName = "Kierunek wiatru: " + ReturnWindDirectionName((item.Element("wind").Element("direction").Attribute("code").Value)),
+                                       CloudsName = "Niebo: " + (item.Element("clouds").Attribute("name").Value)
+                                   }).ToList();
+                    CWeather = tmpList[0];
                 }
             }
         }
@@ -112,6 +201,23 @@ namespace WeatherTarczykowski
         private void TextBlock_Tapped(object sender, TappedRoutedEventArgs e)
         {
             textBoxCity.Text = ((TextBlock)sender).Text;
+        }
+        public CurrentWeather CWeather
+        {
+            get { return currentWeather; }
+            set
+            {
+                if (value != currentWeather)
+                {
+                    currentWeather = value;
+                    OnPropertyChanged("CWeather");
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
